5e6ff434ece6e1e9a72afb80778d85e1d2f7c39b
Ranjeet Singh <raj.sco@gmail.com>
2021-05-20 17:11:40 +0000

    add lebel Department Label

--- main.tf
+++ main.tf
@@ -40,6 +40,7 @@ resource "google_compute_instance" "hashicat" {
   zone         = "${var.region}-b"
   machine_type = var.machine_type
   billable = true
+  department = HR
 
   boot_disk {
     initialize_params {
